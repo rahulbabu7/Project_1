f72ee5349cbb81f99bf37613af0f07fdf25120de
rahulbabu7 <rahulbabu436@gmail.com>
2022-08-26 20:23:37 +0530

    simple score board

---
+++ script.js
@@ -0,0 +1,19 @@
+ let score=0;
+ let score2 = 0;
+
+//let score1 = document.getElementById("score1").value;
+//let score2 = document.getElementById("score2").value;
+
+
+function scores(scores){
+    let value = document.getElementById("score1");   //* gETTING THE value in the id score1 and stores its value to value
+    score+=scores;    
+    value.innerHTML= score;  //*changing the value of score1 id to score
+     
+}
+
+function guest(sc){
+    let value1 = document.getElementById("score22");
+    score2+=sc;
+    value1.innerHTML= score2;
+}
